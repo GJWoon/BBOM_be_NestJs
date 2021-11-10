5c1c9ea9d5d3617aec83a23844124c8c623a2303
GJWoon <GJWoon@github.com>
2021-11-11 00:43:43 +0900

    2021-11-11 ADD ResponseInterCeptor & UserResisterAPI

---
+++ src/common/transform-reponse.ts
@@ -0,0 +1,21 @@
+import { CallHandler, ExecutionContext, HttpServer, HttpStatus, Injectable, NestInterceptor } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { map, Observable } from "rxjs";
+
+export interface Response<T> {
+    data: T;
+}
+@Injectable()
+export class ResponseInterCeptor<T> implements NestInterceptor<T, Response<T>> {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+        return next.handle().pipe(map(data => (this.mapReponse(data))));
+    }
+    
+
+    mapReponse(data: any) {
+        return {
+            data: data,
+            status: HttpStatus.OK
+        }
+    }
+}
